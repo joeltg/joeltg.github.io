7441acaf3f2c703f6f676276324f7120ce423a03
Joel Gustafson <joelg@mit.edu>
2016-10-11 23:03:55 -0400

    added oscar

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -4,6 +4,8 @@
 
 var quotes = [
     "Take wrong turns. Talk to strangers. Do things without always knowing how they'll turn out.\nAnd remember that you are always making up the future as you go.",
+    "Most people are other people. Their thoughts are someone else's opinions, their lives a mimicry, their passions a quotation.",
+    "A foolish consistency is the hobgoblin of little minds."
 ];
 
 var quote = document.getElementById('quote');
